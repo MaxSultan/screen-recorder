2b4a2b57ac1310bf10b814bd8f8fdd4c64927a86
Maxwell Sultan <maxsultankent@gmail.com>
2024-08-02 18:47:10 -0600

    feature: screen record and record webcam in single stream

--- main.js
+++ main.js
@@ -1,24 +1,216 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.querySelector("#app").innerHTML = `
+  <main>
+	<div id="mediaWrapper"></div>
+	<div id="buttonWrapper">
+    <select id="recordOptions">
+      <option value="both" selected>
+        Record Both Webcam and Screen
+      </option>
+    </select>
+    <button id="record">
+      <div></div>
+    </button>
+		<button id="stopRecording" title="Stop Recording and Download Resulting Stream">Stop Recording and Download Resulting Stream</button>
+		<button id="stopAllStreams" title="Stop All Streams">Stop All Streams</button>
+	</div>
+</main>
+`;
 
-setupCounter(document.querySelector('#counter'))
+//#region constants
+let localCamStream,
+  localScreenStream,
+  localOverlayStream,
+  rafId,
+  mediaRecorder,
+  audioContext,
+  audioDestination,
+  overlay;
+let mediaWrapperDiv = document.getElementById("mediaWrapper");
+let stopRecordingBtn = document.getElementById("stopRecording");
+let stopAllStreamsBtn = document.getElementById("stopAllStreams");
+let canvasElement = document.createElement("canvas");
+let canvasCtx = canvasElement.getContext("2d");
+let encoderOptions = { mimeType: "video/webm; codecs=vp9" };
+let recordedChunks = [];
+
+const recordButton = document.getElementById("record");
+
+//#endregion constants
+
+/**
+ * Internal Polyfill to simulate
+ * window.requestAnimationFrame
+ * since the browser will kill canvas
+ * drawing when tab is inactive
+ */
+const requestVideoFrame = function (callback) {
+  return window.setTimeout(function () {
+    callback(Date.now());
+  }, 1000 / 60); // 60 fps - just like requestAnimationFrame
+};
+
+/**
+ * Internal polyfill to simulate
+ * window.cancelAnimationFrame
+ */
+const cancelVideoFrame = (id) => {
+  clearTimeout(id);
+};
+
+const createVideoElement = (id, stream) => {
+  let videoElem = document.createElement("video");
+  videoElem.id = id;
+  videoElem.width = 640;
+  videoElem.height = 360;
+  videoElem.autoplay = true;
+  videoElem.setAttribute("playsinline", true);
+  videoElem.srcObject = new MediaStream(stream.getTracks());
+  return videoElem;
+};
+
+const startWebcam = () =>
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: { deviceId: { ideal: "communications" } },
+  });
+
+const startScreenShare = async () =>
+  navigator.mediaDevices.getDisplayMedia({
+    video: true,
+    audio: true,
+  });
+
+const stopAllStreams = () => {
+  [
+    ...(localCamStream ? localCamStream.getTracks() : []),
+    ...(localScreenStream ? localScreenStream.getTracks() : []),
+    ...(localOverlayStream ? localOverlayStream.getTracks() : []),
+  ].map((track) => track.stop());
+  localCamStream = null;
+  localScreenStream = null;
+  localOverlayStream = null;
+  cancelVideoFrame(rafId);
+  mediaWrapperDiv.innerHTML = "";
+};
+
+const makeComposite = (videoCamElement, videoScreenElement) => () => {
+  // Theres an issue in here where these videos have no width and height so the canvas has no width and height
+  if (videoCamElement && videoScreenElement) {
+    console.log(videoScreenElement.videoHeight, videoScreenElement.videoWidth);
+    canvasCtx.save();
+    canvasElement.setAttribute("width", `${500}px`);
+    canvasElement.setAttribute("height", `${500}px`);
+    canvasCtx.clearRect(0, 0, 500, 500);
+    canvasCtx.drawImage(videoScreenElement, 0, 0, 500, 500);
+
+    canvasCtx.drawImage(
+      videoCamElement,
+      0,
+      Math.floor(500 - 500 / 4),
+      Math.floor(500 / 4),
+      Math.floor(500 / 4)
+    );
+
+    let imageData = canvasCtx.getImageData(0, 0, 500, 500);
+
+    canvasCtx.putImageData(imageData, 0, 0);
+    canvasCtx.restore();
+  }
+  rafId = requestVideoFrame(makeComposite(videoCamElement, videoScreenElement));
+};
+
+const mergeStreams = async (localCamStream, localScreenStream) => {
+  const videoCamElement = createVideoElement("camStream", localCamStream);
+  videoCamElement.style.display = "hidden";
+  videoCamElement.volume = 0;
+  videoCamElement.oncanplay = () => {
+    videoCamElement.play();
+  };
+
+  const videoScreenElement = createVideoElement(
+    "screenStream",
+    localScreenStream
+  );
+  videoScreenElement.style.display = "hidden";
+  videoScreenElement.volume = 0;
+
+  videoScreenElement.oncanplay = () => {
+    videoScreenElement.play();
+  };
+
+  makeComposite(videoCamElement, videoScreenElement)();
+  audioContext = new AudioContext();
+  audioDestination = audioContext.createMediaStreamDestination();
+  let fullVideoStream = canvasElement.captureStream();
+  [
+    ...(localCamStream ? localCamStream.getAudioTracks() : []),
+    ...(localScreenStream ? localScreenStream.getAudioTracks() : []),
+  ]
+    .map((track) => new MediaStream([track]))
+    .map((track) => audioContext.createMediaStreamSource(track))
+    .map((track) => track.connect(audioDestination));
+
+  localOverlayStream = new MediaStream([...fullVideoStream.getVideoTracks()]);
+  let fullOverlayStream = new MediaStream([
+    ...fullVideoStream.getVideoTracks(),
+    ...audioDestination.stream.getTracks(),
+  ]);
+
+  if (localOverlayStream) {
+    overlay = createVideoElement("pipOverlayStream", localOverlayStream);
+    mediaWrapperDiv.appendChild(overlay);
+    mediaRecorder = new MediaRecorder(fullOverlayStream, encoderOptions);
+    mediaRecorder.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        recordedChunks.push(event.data);
+        download(recordedChunks);
+      }
+    };
+  }
+};
+
+const startRecording = () => {
+  mediaRecorder.start();
+  // document.getElementById("pipOverlayStream").style.border = "2px solid red";
+};
+
+const download = (blobData) => {
+  var blob = new Blob(blobData, {
+    type: "video/webm",
+  });
+  var url = URL.createObjectURL(blob);
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.href = url;
+  a.download = "result.webm";
+  a.click();
+  window.URL.revokeObjectURL(url);
+};
+
+const stopRecording = () => {
+  stopAllStreams();
+  mediaRecorder?.stop();
+};
+
+const handleRecord = async () => {
+  const recordOption = document.getElementById("recordOptions").value;
+  if (recordOption === "both") {
+    Promise.allSettled([startWebcam(), startScreenShare()]).then(
+      ([camStream, screenStream]) => {
+        localCamStream = camStream.value;
+        localScreenStream = screenStream.value;
+        mergeStreams(localCamStream, localScreenStream).then(() =>
+          startRecording()
+        );
+      }
+    );
+  }
+};
+
+stopAllStreamsBtn.addEventListener("click", stopAllStreams);
+stopRecordingBtn.addEventListener("click", stopRecording);
+
+recordButton.addEventListener("click", handleRecord);
--- notes.md
+++ notes.md
@@ -3,3 +3,38 @@
 a app that allows you to record your screen and your video at the same time with one audio and lets you save videos
 
 10 hours
+
+9 hours left
+
+Basic goal (2 hours):
+
+- Webcam video recording
+  [X] webcam can start and stop recording
+  [X] recording can be downloaded
+  [X] recording can be previewed while recording
+  [] recording can be previewed after recording
+
+Basic Goal #2
+
+- Screen Capture
+  [] the user can record a portion of or all of the screen
+  [] the user gets a visual indicator to show that recording is in progress
+  []
+
+End goal
+Preview of screen recording and video
+Record button that records screen section, webcam video, and microphone audio at the same time
+Download/Save that allows you to access they system dialog for saving a video or downloads it to the browser
+
+Core Idea:
+Fetch Webcam Stream via getUserMedia()
+Fetch Screen Share Stream via getDisplayMedia()
+Merge Both Stream using some math & canvas operations
+Use canvas.captureStream() to generate the composite video stream.
+Use AudioContext to merge audio clips (especially needed if using both microphone & system audio).
+Use MediaStream constructor to create a new stream using - the video from the new stream + audio from audioContext Destination Node as follows -
+
+new MediaStream([...newStream.getVideoTracks(), ...audioDestination.stream.getTracks()]);
+
+Use newly generated MediaStream as required (i.e. replace in RTCPeerConnection, etc.).
+In this example - MediaRecorder API is used to record the resulting composite/picture-in-picture video. Recording begins when the "Record Resulting Stream" button is clicked. The final result can be downloaded upon clicking the "Stop Recording and Download Resulting Stream" button
